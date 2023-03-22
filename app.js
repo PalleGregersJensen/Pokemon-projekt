961a2ed18e5eb4e4941ade831f83af2975b8d39c
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-03-22 21:52:07 +0100

    Diverse småting tilføjet
    Fungerer ikke ehelt. Se Potter-app for hjælp

--- app.js
+++ app.js
@@ -1,5 +1,108 @@
-// Datamodel for Pokemon er som følger:
+"use strict";
 
+window.addEventListener("load", initApp);
+
+console.log("JS kører");
+
+function initApp() {
+  
+  const wattrel = {
+    name: "Wattrel",
+    description:
+      "When its wings catch the wind, the bones within produce electricity. This Pokémon dives into the ocean, catching prey by electrocuting them",
+    ability: "volt absorb, wind power",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/940.png",
+    footprint: undefined,
+    dexindex: 940,
+    type: "electric, flying",
+    subtype: undefined,
+    weaknesses: "ice, rock",
+    gender: "male/female",
+    weight: 3600,
+    height: 40,
+    generation: undefined,
+    spilversion: undefined,
+    canEvolve: true,
+    statsHP: 2,
+    statsAttack: 2,
+    statsDefence: 2,
+    statsSpecialAttack: 3,
+    statsSpecialDefence: 2,
+    statsSpeed: 3,
+  };
+
+  console.log(wattrel);
+
+  function fetchJSON () {
+  
+}
+
+  function showList() {
+    
+  }
+  
+  function showPokemon(pokemon) {
+    const html =
+      /*html*/
+      `<h2>Name: ${pokemon.name}</h2> 
+     <p><img src="${pokemon.image}"></p> 
+  `;
+
+    document.querySelector("#pokemons").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#pokemons article:last-child").addEventListener("click", pokemonClicked);
+
+   
+  }
+  function pokemonClicked() {
+    console.log(pokemon);
+    document.querySelector("dialog").showModal();
+    // change elements in dialog
+    document.querySelector("#dialog-name").textContent = character.name;
+    document.querySelector("#dialog-image").src = character.image;
+    // show dialog
+    document.querySelector("#dialog-character").showModal();
+  }
+
+ showPokemon(wattrel);
+
+}
+
+
+// -------------Elementer, der fremhæves via textContent i Potter-app------ 
+// document.querySelector("#dialog-birthday").textContent = character.dateOfBirth;
+// document.querySelector("#dialog-species").textContent = character.species;
+// document.querySelector("#dialog-gender").textContent = character.gender;
+// document.querySelector("#dialog-ancestry").textContent = character.ancestry;
+// document.querySelector("#dialog-eyecolor").textContent = character.eyeColour;
+// document.querySelector("#dialog-haircolor").textContent = character.hairColour;
+// document.querySelector("#dialog-wand").textContent = character.wand;
+// document.querySelector("#dialog-patrous").textContent = character.patronus;
+// document.querySelector("#dialog-hogwartsStudent").textContent =character.hogwartsStudent;
+// document.querySelector("#dialog-hogwartsStaff").textContent =character.hogwartsStaff;
+// document.querySelector("#dialog-actor").textContent = character.actor;
+// document.querySelector("#dialog-alive").textContent = character.alive;
+
+
+
+// -----Wattrel properties--------------
+/* <li>Dexindex: ${pokemon.dexindex}</li> */
+// <li>Type: ${pokemon.type}</li>
+// <li>Subtype: ${pokemon.subtype}</li>
+// <li>Weaknesses: ${pokemon.weaknesses}</li>
+// <li>Gender: ${pokemon.gender}</li>
+// <li>Weight: ${pokemon.weight}</li>
+// <li>Height: ${pokemon.height}</li>
+// <li>Generation: ${pokemon.generation}</li>
+// <li>Gameversion: ${pokemon.spilversion}</li>
+// <li>Can evolve: ${pokemon.canEvolve}</li>
+// <li>HP: ${pokemon.statsHP}</li>
+// <li>Attack: ${pokemon.statsAttack}</li>
+// <li>Defense: ${pokemon.statsDefence}</li>
+// <li>Special attack: ${pokemon.statsSpecialAttack}</li>
+// <li>Special defence: ${pokemon.statsSpecialDefence}</li>
+//  <li>Speed: ${pokemon.statsSpeed}</li>
+
+// --------Datamodel for Pokemon er som følger:
 // name: tekst
 // description: tekst
 // ability: tekst
@@ -24,61 +127,3 @@
 // properties kan være null eller undefined,
 // men alle skal være der, og have den type der er angivet -
 // altså, string, number, boolean, etc.eller null / undefined
-
-"use strict";
-
-console.log("JS kører");
-
-const wattrel = {
-  name: "Wattrel",
-  description:
-    "When its wings catch the wind, the bones within produce electricity. This Pokémon dives into the ocean, catching prey by electrocuting them",
-  ability: "volt absorb, wind power",
-  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/940.png",
-  footprint: undefined,
-  dexindex: 940,
-  type: "electric, flying",
-  subtype: undefined,
-  weaknesses: "ice, rock",
-  gender: "male/female",
-  weight: 3600,
-  height: 40,
-  generation: undefined,
-  spilversion: undefined,
-  canEvolve: true,
-  statsHP: 2,
-  statsAttack: 2,
-  statsDefence: 2,
-  statsSpecialAttack: 3,
-  statsSpecialDefence: 2,
-  statsSpeed: 3,
-};
-
-console.log(wattrel);
-
-function showPokemon(pokemon) {
-  const html =
-    /*html*/
- `<li>Name: ${pokemon.name}</li> 
-  <li><img src="${pokemon.image}"></li> 
-  <li>Dexindex: ${pokemon.dexindex}</li>
-  <li>Type: ${pokemon.type}</li> 
-  <li>Subtype: ${pokemon.subtype}</li> 
-  <li>Weaknesses: ${pokemon.weaknesses}</li>
-  <li>Gender: ${pokemon.gender}</li> 
-  <li>Weight: ${pokemon.weight}</li> 
-  <li>Height: ${pokemon.height}</li>
-  <li>Generation: ${pokemon.generation}</li> 
-  <li>Gameversion: ${pokemon.spilversion}</li> 
-  <li>Can evolve: ${pokemon.canEvolve}</li>
-  <li>HP: ${pokemon.statsHP}</li> 
-  <li>Attack: ${pokemon.statsAttack}</li>
-  <li>Defense: ${pokemon.statsDefence}</li> 
-  <li>Special attack: ${pokemon.statsSpecialAttack}</li> 
-  <li>Special defence: ${pokemon.statsSpecialDefence}</li>
-   <li>Speed: ${pokemon.statsSpeed}</li>
-  `;
-  document.querySelector("#pokemon").insertAdjacentHTML("beforeend", html);
-}
-
-showPokemon(wattrel);
